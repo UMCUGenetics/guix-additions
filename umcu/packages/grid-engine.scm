ae036547c36228cd6c8876e0d6e2c940367d866a
Roel Janssen <roel@gnu.org>
2017-04-11 19:30:27 +0200

    Add grid-engine command-line utilities and libdrmaa.

---
+++ umcu/packages/grid-engine.scm
@@ -0,0 +1,156 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2016, 2017 Roel Janssen <roel@gnu.org>
+;;;
+;;; This file is not officially part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (umcu packages grid-engine)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages gawk)
+  #:use-module (gnu packages java)
+  #:use-module (gnu packages mpi)
+  #:use-module (gnu packages ruby)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages tcl)
+  #:use-module (gnu packages tls))
+
+(define-public grid-engine-core
+  (package
+    (name "grid-engine-core")
+    (version "8.1.9")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "http://arc.liv.ac.uk/downloads/SGE/releases/"
+                    version "/sge-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0ra7m9mf09zzcf815y3zqzqkj95v9zm24nhhvzmdh2bsqgdmk59w"))))
+    (build-system gnu-build-system)
+    (supported-systems '("x86_64-linux"))
+    (inputs
+     `(("bdb" ,bdb)
+       ("tcsh" ,tcsh)
+       ("inetutils" ,inetutils)
+       ("hwloc" ,hwloc)
+       ("openssl" ,openssl)
+       ("coreutils" ,coreutils)
+       ("tcl" ,tcl)
+       ("linux-pam" ,linux-pam)
+       ("python" ,python-2.7)
+       ("perl" ,perl)
+       ("ruby" ,ruby)
+       ("gawk" ,gawk)
+       ("icedtea" ,icedtea)))
+    (arguments
+     `(#:tests? #f
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'patch-various-stuff
+           (lambda* (#:key inputs #:allow-other-keys)
+             (substitute* "source/aimk"
+               (("/usr/bin/uname") "uname")
+               (("uname") (string-append (assoc-ref inputs "coreutils")
+                                         "/bin/uname")))
+             (substitute* "source/dist/util/arch"
+               (("/bin/uname") (string-append (assoc-ref inputs "coreutils")
+                                              "/bin/uname"))
+               (("/lib64/libc.so.6") (string-append (assoc-ref inputs "libc")
+                                                    "/lib/libc.so.6"))
+               (("awk") (string-append (assoc-ref inputs "gawk") "/bin/gawk"))
+               (("head") (string-append (assoc-ref inputs "coreutils")
+                                        "/bin/head")))
+             (substitute* "source/aimk"
+               (("= cc") (string-append "= gcc")))
+             (substitute* "source/configure"
+               (("SHELL=") (string-append "SHELL=" (assoc-ref inputs "bash")
+                                          "/bin/sh #")))
+             ))
+         (replace 'configure
+           (lambda* (#:key inputs #:allow-other-keys)
+             (chdir "source")
+             (setenv "SGE_INPUT_CFLAGS"
+                     (string-append "-I" (assoc-ref inputs "openssl") "/include"))
+             (setenv "JAVA_HOME" (assoc-ref inputs "icedtea"))
+             (system "scripts/bootstrap.sh")
+             #t))
+         (replace 'build
+           (lambda _
+             (system "./aimk -only-core -no-java -no-jni")
+             #t))
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             ;; The scripts/distinst would not work, so we copy the files
+             ;; over manually.
+             (chdir "LINUXAMD64")
+             (let ((bin (string-append (assoc-ref outputs "out") "/bin"))
+                   (lib (string-append (assoc-ref outputs "out") "/lib")))
+               (mkdir-p bin)
+               (mkdir-p lib)
+
+               ;; Binaries
+               (for-each (lambda (file)
+                           (install-file file bin))
+                         '("qacct" "qalter" "qconf" "qdel" "qevent" "qhost"
+                           "qmod" "qping" "qquota" "qrdel" "qrstat" "qrsub"
+                           "qsh" "qstat" "qsub" "sge_coshepherd" "sge_execd"
+                           "sgepasswd" "sge_qmaster" "sge_shadowd"
+                           "sge_share_mon" "sge_shepherd"))
+               (system* "ln" "--symbolic"
+                        (string-append bin "/qalter")
+                        (string-append bin "/qhold"))
+               (system* "ln" "--symbolic"
+                        (string-append bin "/qalter")
+                        (string-append bin "/qresub"))
+               (system* "ln" "--symbolic"
+                        (string-append bin "/qalter")
+                        (string-append bin "/qrls"))
+               (system* "ln" "--symbolic"
+                        (string-append bin "/qsh")
+                        (string-append bin "/qrsh"))
+               (system* "ln" "--symbolic"
+                        (string-append bin "/qstat")
+                        (string-append bin "/qselect"))
+               (system* "ln" "--symbolic"
+                        (string-append bin "/qlogin")
+                        (string-append bin "/qsh"))
+
+               ;; Libraries
+               (for-each (lambda (file)
+                           (install-file file lib))
+                         '("libdrmaa.so" "libjuti.so" "libspoolb.so"
+                           "libspoolc.so" "pam_sge_authorize.so"
+                           "pam_sge-qrsh-setup.so"))
+               (system* "ln" "--symbolic"
+                        (string-append lib "/libdrmaa.so")
+                        (string-append lib "/libdrmaa.so.1"))
+               #t))))))
+    (home-page "")
+    (synopsis "")
+    (description "")
+    (license (list license:asl2.0 license:gpl2+))))
