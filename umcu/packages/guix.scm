96e0ae5988ba7c294edab006dc9d7eb6cbd86300
Roel Janssen <roel@gnu.org>
2017-04-26 15:24:03 +0200

    Add collectl.
    * umcu/packages/guix.scm (collectl):  New variable.

--- umcu/packages/guix.scm
+++ umcu/packages/guix.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2016 Roel Janssen <roel@gnu.org>
+;;; Copyright © 2016, 2017 Roel Janssen <roel@gnu.org>
 ;;;
 ;;; This file is not officially part of GNU Guix.
 ;;;
@@ -17,13 +17,16 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (umcu packages guix)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
+  #:use-module (guix download)
   #:use-module (guix build utils)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages package-management)
+  #:use-module (gnu packages perl)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages networking))
 
@@ -144,3 +147,39 @@ has been slightly modified to work on the UMC Utrecht cluster set-up.")
     (description "This script is a wrapper around iotop, to gather reads and
 writes to the disk for various processes.")
     (license #f)))
+
+(define-public collectl
+  (package
+   (name "collectl")
+   (version "4.1.2")
+   (source (origin
+             (method url-fetch)
+             (uri (string-append
+                   "mirror://sourceforge/collectl/collectl/collectl-" version
+                   "/collectl-" version ".src.tar.gz"))
+             (sha256
+              (base32
+               "0cf44kwazxfh98yshyi1jcw34lzfy5ahqxpgqqsxa6ps7zlm89lp"))))
+   (build-system gnu-build-system)
+   (arguments
+    `(#:tests? #f ; There are no tests.
+      #:phases
+      (modify-phases %standard-phases
+        (delete 'build) ; There's nothing to build.
+        (replace 'configure
+          (lambda* (#:key outputs #:allow-other-keys)
+            (substitute* "INSTALL"
+              (("DESTDIR:=\"/\"") (format #f "DESTDIR:=~s"
+                                          (assoc-ref outputs "out")))
+              (("DESTDIR/usr") "DESTDIR"))))
+        (replace 'install
+                 (lambda _
+                   (system* "./INSTALL"))))))
+   (inputs
+    `(("perl" ,perl)))
+   (home-page "http://collectl.sourceforge.net")
+   (synopsis "Performance data collector")
+   (description "This package provides a program that collects various
+performance measurement data like CPU, memory, disk and network performance
+numbers.")
+   (license license:artistic2.0)))
