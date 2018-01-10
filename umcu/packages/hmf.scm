UMCUGenetics/guix-additions

commit 61d8c537a892082b03c27ed83f8072469a420d90

hmf: Over-allocate GATK-Queue jobs by 4GB.
* umcu/packages/hmf.scm (hmf-pipeline): Over-allocate GATK-Queue jobs by 4GB.

umcu/packages/hmf.scm
@@ -1127,13 +1127,17 @@ REPORT_STATUS	~a"
                (("rcopy \\$slice_dir") "$File::Copy::Recursive::KeepMode = 0; rcopy $slice_dir"))
 
              (substitute* "HMF/Pipeline/Sge.pm"
-               ;; Over-allocate by 2G for each job, because some SGE
+               ;; Over-allocate by 4G for each job, because some SGE
                ;; implementations have memory overhead on each job.
                (("my \\$qsub = generic\\(\\$opt, \\$function\\) . \" -m a")
                 "my $h_vmem = (4 + $opt->{$function.\"_MEM\"}).\"G\"; my $qsub = generic($opt, $function) . \" -m a -V -l h_vmem=$h_vmem")
                ;; Make sure that environment variables are passed along
                ;; to the jobs correctly.
-               (("qsub -P") "qsub -V -P")))))))
+               (("qsub -P") "qsub -V -P")
+               ;; Also apply the 4GB over-allocation to GATK-Queue-spawned jobs.
+               (("my \\$qsub = generic\\(\\$opt, \\$function\\);")
+                "my $h_vmem = (4 + $opt->{$function.\"_MEM\"}).\"G\"; my $qsub = generic($opt, $function) . \" -V -l h_vmem=$h_vmem\"")
+                ))))))
     (inputs
      `(("bammetrics" ,bammetrics)
        ("bamutils" ,bamutils)

commit 5a5d4f19834ad43e6ec406c7e5f5a0e409970c37

hmf: hmf-pipeline: Fix syntax error introduced in 61d8c53.
* umcu/packages/hmf.scm (hmf-pipeline): Add semicolon to end of line.

umcu/packages/hmf.scm
@@ -1136,7 +1136,7 @@ REPORT_STATUS	~a"
                (("qsub -P") "qsub -V -P")
                ;; Also apply the 4GB over-allocation to GATK-Queue-spawned jobs.
                (("my \\$qsub = generic\\(\\$opt, \\$function\\);")
-                "my $h_vmem = (4 + $opt->{$function.\"_MEM\"}).\"G\"; my $qsub = generic($opt, $function) . \" -V -l h_vmem=$h_vmem\"")
+                "my $h_vmem = (4 + $opt->{$function.\"_MEM\"}).\"G\"; my $qsub = generic($opt, $function) . \" -V -l h_vmem=$h_vmem\";")
                 ))))))
     (inputs
      `(("bammetrics" ,bammetrics)