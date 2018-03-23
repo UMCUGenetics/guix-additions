32c5717a13fbf5e27a5a225a1fe426149a8a55e2
Roel Janssen <roel@gnu.org>
2018-03-23 11:36:50 +0100

    Fix the update.sh hook.
    * update.sh: Add SGE_DUMMY=1.

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/gnu/profiles/per-service/hpcguix-web/bin/bash
 
+export SGE_DUMMY=1
 cd /gnu/repositories/guix-additions;
 /gnu/profiles/base/bin/guixr load-profile /gnu/profiles/per-service/hpcguix-web -- <<EOF
   git reset --hard HEAD;
