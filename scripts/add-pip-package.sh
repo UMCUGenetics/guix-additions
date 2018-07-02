7e6fcef8c24b5ab11c6b0d64755f103d377b4e67
Joep de Ligt <joepio@gmail.com>
2018-07-02 11:45:38 +0200

    Create add-pip-package.sh

---
+++ scripts/add-pip-package.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# JdL
+
+# Based on a list of pip packages
+# For each package get dependencies
+#   get/create recipes for dependencies
+# create recipie for package 
+# Add all GUIX packages to guix-addiotions
+# Make a request to upstream to include the recipies
+
+# Use guix import pypi xx where possible
+# 
+
