e032ae046476264fb10336d2faf5bd6c3c3564a8
Roel Janssen <roel@gnu.org>
2018-07-04 10:49:07 +0200

    datasets: Update hash/version for the GRCh37 edition of clinvar.
    * umcu/packages/datasets.scm (clinvar-grch37): Update version.

--- umcu/packages/datasets.scm
+++ umcu/packages/datasets.scm
@@ -71,14 +71,14 @@ as efficiently and effectively as possible.")
 
 (define-public clinvar-grch37
   (package (inherit clinvar)
-    (version "GRCh37-20180225")
+    (version "GRCh37-20180704")
     (source (origin
              (method url-fetch)
              (uri (string-append
                    "ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz"))
              (sha256
               (base32
-               "0lm51ma4mi172gklzrbd6f9z8432ry0w82r5g2mdldcc9fsi62m1"))))
+               "1sajsjbsp0288g03h4r2zay7nigjzqb41n2bmg324klczdxvg0fk"))))
    (arguments
     `(#:modules ((guix build utils))
       #:builder
