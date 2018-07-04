3d6564ad1a4f6a5415066299fd9d8453ba3eafab
jdeligt <joepio@gmail.com>
2018-07-04 17:35:00 +0200

    Adding native-inputs for test building

--- scripts/add-pip-package.py
+++ scripts/add-pip-package.py
@@ -28,6 +28,7 @@ GUIX_PACKAGE_NOTFOUND_MESSAGE="package not found"
 #error: Could not find suitable distribution for Requirement.parse('xxxx<=0.0.0')
 # present in error output
 GUIX_DEPENDENCY_NOTFOUND_PATTERN="(?<=Could not find suitable distribution for Requirement.parse\(\')(.+)\'"
+PYTHON_MODULE_NOTFOUND_PATTERN="(?<=ModuleNotFoundError: No module named \')(.+)\'"
 GUIX_PACKAGE_PATH="GUIX_PACKAGE_PATH"
 
 # ASSUMES TO BE RUN FROM THE SCRIPTS FOLDER
@@ -95,6 +96,15 @@ def add_recipe(package, packagefile):
 # (arguments
 # '(#:tests? #f))
 
+def add_native(recipe, dependency):
+    # Add inputs field if not yet present
+    if not "(native-inputs" in recipe:
+        recipe = recipe.replace("(build-system python-build-system)","(build-system python-build-system)\n(native-inputs\n\'())")
+
+    # Add dependency
+    recipe = recipe.replace("(native-inputs\n\'(", "(native-inputs\n\'((\"{0}\" ,{0})\n".format(dependency))
+
+
 def add_dependency(recipe, dependency):
     # Add inputs field if not yet present
     if not "(inputs" in recipe:
@@ -149,20 +159,28 @@ def make_recipe(package):
 
         while "Could not find suitable distribution" in stderr:
             # parse missing dependency from buildstate
-            missing_dep = re.match(GUIX_DEPENDENCY_NOTFOUND_PATTERN, stderr)
-            if missing_dep is None:
+            missing_dep = re.findall(GUIX_DEPENDENCY_NOTFOUND_PATTERN, stderr)
+            missing_nat = re.findall(PYTHON_MODULE_NOTFOUND_PATTERN, stderr)
+
+            if len(missing_dep) is 0 and len(missing_nat) is 0:
                 if args.verbose: print("Package [{0}] - No more missing dependecies".format(package))
                 break
-            if args.verbose: print("Package [{0}] - Missing dependency {1}".format(package, missing_dep.groups()))
 
-            # recurse for this dependency
-            missing_dep = missing_dep.group(1).split("=")[0].replace("<","").replace(">","")
-            make_recipe(missing_dep)
+            # For alll missing dependencies
+            for mis_dep in missing_dep:
+                if args.verbose: print("Package [{0}] - Missing dependency {1}".format(package, mis_dep))
+                # recurse for this dependency
+                missing_dep = mis_dep.split("=")[0].replace("<","").replace(">","")
+                make_recipe(missing_dep)
+                add_dependency(packagerecipe, missing_dep)
 
-            # when dependency is done add it and continue
-            add_dependency(packagerecipe)
+            for mis_nat in missing_nat:
+                if args.verbose: print("Package [{0}] - Missing native input {1}".format(package, mis_nat))
+                make_recipe(mis_nat)
+                add_native(packagerecipe, mis_nat)
+
+
             if args.verbose: print("Package [{0}] - Current recipe {1}".format(package, packagerecipe))
-
             with open(recipefile, 'w') as outf:
                 outf.write(packagerecipe)
 
@@ -170,8 +188,10 @@ def make_recipe(package):
             stdout, stderr = p_build.communicate()
             if args.verbose: print("Package [{0}] - Current buildstate\n O:{1}\n E:{2}".format(package, stdout, stderr))
 
+        exit(1)
         # We have a complete and working packagerecipe
         add_recipe(package, recipefile)
+
         # Check if it installs
         p_pack = Popen("guixr package -i {}".format(package), stdout=PIPE, stderr=PIPE, shell=True)
         stdout, stderr = p_pack.communicate()
