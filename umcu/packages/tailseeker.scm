c976e68a2770fa0f72616ba40d4f1f780b8d47f5
Roel Janssen <roel@gnu.org>
2019-01-07 14:39:09 +0100

    Add tailseeker v2.
    * Makefile.am: Register tailseeker module.
    * umcu/packages/tailseeker.scm: New file.

---
+++ umcu/packages/tailseeker.scm
@@ -0,0 +1,123 @@
+;;; Copyright © 2019 Roel Janssen <roel@gnu.org>
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (umcu packages tailseeker)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system python)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bioinformatics)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages machine-learning)
+  #:use-module (gnu packages maths))
+
+(define-public all-your-base
+  (let ((commit "b44212c66fe95e0b5ca51bfefcbe4eaa2c178fc4"))
+    (package
+     (name "all-your-base")
+     (version (string-append "2-" (string-take commit 7)))
+     (source (origin
+               (method git-fetch)
+               (uri (git-reference
+                     (url "https://github.com/hyeshik/AYB2.git")
+                     (commit commit)))
+               (file-name (git-file-name name version))
+               (sha256
+                (base32 "165ncgmw189qw72kjzg08y704sb0a6fi1wihibl2wr30p5091mcb"))))
+     (build-system gnu-build-system)
+     (arguments
+      `(#:tests? #f
+        #:phases
+        (modify-phases %standard-phases
+          (delete 'configure)
+          (add-before 'build 'move-to-src-directory
+            (lambda _
+              (chdir "src")
+              #t))
+          (replace 'install
+            (lambda* (#:key inputs #:allow-other-keys)
+              (let* ((out (string-append (assoc-ref %outputs "out")))
+                     (bin (string-append out "/bin")))
+                (mkdir-p bin)
+                (install-file "../bin/AYB" bin)))))))
+     (inputs
+      `(("bzip2" ,bzip2)
+        ("zlib" ,zlib)
+        ("lapack" ,lapack)))
+     (home-page "https://github.com/hyeshik/AYB2")
+     (synopsis "Base caller for the Illumina platform")
+     (description "This package contains a fork of the All Your Base base 
+caller, specifically targeting the use for tailseeker2.")
+     (license license:gpl3+))))
+
+(define-public tailseeker2
+  (package
+   (name "tailseeker")
+   (version "2.0")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/hyeshik/tailseeker.git")
+                  (commit "0edfa971df6616d3268d25676d2922bc386143a8")))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32 "1767wv5jkhkyqy6z7w2kn4jgkak852yphfppgr8xwf3id30s8bdf"))))
+   (build-system python-build-system)
+   (arguments
+    `(#:tests? #f
+      #:phases
+      (modify-phases %standard-phases
+        (add-before 'build 'move-to-src
+          (lambda* (#:key inputs #:allow-other-keys)
+            (chdir "src")
+            (setenv "CFLAGS" (string-append
+                              "-I"
+                              (assoc-ref inputs "python-numpy")
+                              "/lib/python3.6/site-packages/numpy/core/include"))))
+        (add-after 'install 'install-python-modules
+          (lambda* (#:key inputs outputs #:allow-other-keys)
+            (mkdir (string-append (assoc-ref outputs "out")
+                                  "/lib/python3.6/site-packages/tailseeker"))
+            (copy-recursively "../tailseeker"
+                              (string-append (assoc-ref outputs "out")
+                                             "/lib/python3.6/site-packages/tailseeker")))))))
+   (native-inputs
+    `(("pkg-config" ,pkg-config)))
+   (inputs
+    `(("python" ,python-3.6)))
+   (propagated-inputs
+    `(("python-biopython" ,python-biopython)
+      ("python-scikit-learn" ,python-scikit-learn)
+      ("python-scipy" ,python-scipy)
+      ("python-numpy" ,python-numpy)
+      ("python-pandas" ,python-pandas)
+      ("python-matplotlib" ,python-matplotlib)
+      ("python-pyyaml" ,python-pyyaml)
+      ("ghmm" ,ghmm)
+      ("snakemake" ,snakemake)
+      ("htslib" ,htslib)))
+   (home-page "https://github.com/hyeshik/tailseeker/tree/tailseeker2")
+   (synopsis "Measure poly-A tail lengths from Illumina sequencers")
+   (description "This package contains software for measuring poly(A) tail
+length and 3′-end modifications using a high-throughput sequencer.")
+   (license license:expat)))
