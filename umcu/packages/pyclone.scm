e4a0e55e54409a3a4e6089a4393944536447418d
Roel Janssen <roel@gnu.org>
2019-02-04 13:17:25 +0100

    Reflect upstream's module re-arrangements for python2-pyyaml.
    * umcu/packages/mongodb.scm: Add (gnu packages python-xyz) to module imports.
    * umcu/packages/pyclone.scm: Likewise.
    * umcu/packages/python.scm: Likewise.

--- umcu/packages/pyclone.scm
+++ umcu/packages/pyclone.scm
@@ -23,6 +23,7 @@
   #:use-module (guix build-system python)
   #:use-module (gnu packages)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages bioinformatics))
 
 (define-public python2-pydp
