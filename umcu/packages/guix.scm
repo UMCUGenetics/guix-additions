3f4ba9fef34b47a5593da802bef80ea5719e4c75
Roel Janssen <roel@gnu.org>
2020-09-29 13:12:35 +0200

    Revert "guix: guixr: Use Guix's bash."
    This reverts commit 47b1f695cda658f41443cd254894638e9904fb85.

--- umcu/packages/guix.scm
+++ umcu/packages/guix.scm
@@ -61,7 +61,7 @@
            (lambda* (#:key inputs #:allow-other-keys)
              (with-output-to-file "guixr"
                (lambda _
-                 (format #t "#!~a/bin/bash
+                 (format #t "#!/bin/bash
 
 set -u
 set -e
@@ -170,7 +170,6 @@ elif [ \"$1\" == \"load-profile\" ]; then
 else
   ${guix} $@
 fi~%"
-                         (assoc-ref inputs "bash-full")
                          (assoc-ref inputs "git")
                          (assoc-ref inputs "coreutils")
                          (assoc-ref inputs "grep")
